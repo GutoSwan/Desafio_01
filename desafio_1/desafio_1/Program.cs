1aae5203130c6bf82c24692383c06c08ab379fc6
GutoSwan <gubuenosa@gmail.com>
2022-04-07 16:55:02 -0300

    Desafio 2 concluido

--- desafio_1/desafio_1/Program.cs
+++ desafio_1/desafio_1/Program.cs
@@ -1,1 +1,54 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using desafio_1;
+using System;
+
+namespace Desafio
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Ubuntu ubuntinho = new Ubuntu("chubs silva", true, 65, 1.50, 50);
+            Ubuntu ubuntao = new Ubuntu("orlando santos", false, 24, 1.80, 130);
+            Ubuntu ubuntaco = new Ubuntu("scooby silva", true, 22, 1.90, 70);
+
+            int somaIdades = ubuntinho.idade + ubuntao.idade + ubuntaco.idade;
+            Console.WriteLine(somaIdades);
+            Console.ReadKey();
+
+            String nomePessoa;
+            Console.WriteLine($"{ubuntinho.nome}\n{ubuntao.nome}\n{ubuntaco.nome}");
+
+            Console.WriteLine($"imc do chubs é: {ubuntinho.peso / (ubuntinho.altura * ubuntinho.altura)}");
+            Console.WriteLine($"imc do orlando é: {ubuntao.peso / (ubuntao.altura * ubuntao.altura)}");
+            Console.WriteLine($"imc do scooby é: {ubuntaco.peso/  (ubuntaco.altura*ubuntaco.altura)}");
+
+            var list = new List<Ubuntu>(){
+                ubuntinho,ubuntao,ubuntaco
+            };
+
+            list.ForEach(x =>
+            {
+                if(x.dev == true)
+                {
+                    Console.WriteLine($"{x.nome} é dev");
+                }
+                else
+                {
+                    Console.WriteLine($"{x.nome} não é dev");
+                }
+                
+
+                if (x.nome.Contains("silva"))
+                {
+                    Console.WriteLine($"{x.nome} é silva");
+
+                }
+                else
+                {
+                    Console.WriteLine($"{x.nome} não é silva");
+                }
+            });
+        }
+    }
+ 
+}
---
+++ desafio_1/desafio_1/Ubuntu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio_1
+{
+    public class Ubuntu
+    {
+        public string nome { get; set; }
+        public bool dev { get; set; }
+        public int idade { get; set; }
+        public double altura { get; set; }
+        public double peso { get; set; }
+
+        public Ubuntu(string nome, bool dev, int idade, double altura, double peso)
+        {
+            this.nome = nome;
+            this.dev = dev;
+            this.idade = idade;
+            this.altura = altura;
+            this.peso = peso;
+        }
+    }
+}
